90b550aa5f05431c048579681a309352105a0673
micky-code <solaisan22@.com>
2024-10-25 11:53:37 +0700

    update

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,12 +1,17 @@
 <template>
-    <h1 class="text-3xl font-bold underline bg-red-600">
-  Hello world!
-</h1>
-  </template>
-  
-  <script>
-  export default {
-    name: 'MyComponent',
-  }
-  </script>
-  
+  <div>
+    <h1 class="text-3xl font-bold underline bg-red-600">Hello world!</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias
+      adipisci ipsa asperiores at repudiandae nam natus voluptatum? Aspernatur
+      impedit ut numquam sed optio, officiis distinctio vel, illum ducimus
+      quaerat nulla.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+};
+</script>
