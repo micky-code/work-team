71a0dbfd242a5c236e98c82fc15fc66cee9a90b9
micky-code <solaisan22@.com>
2024-10-25 11:36:04 +0700

    first commit

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1 class="text-3xl font-bold underline bg-red-600">
+  Hello world!
+</h1>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyComponent',
+  }
+  </script>
+  
